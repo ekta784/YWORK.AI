30e703c8f0f0f8f1e033bd886308eabdda7de084
ekta784 <ektavora0708@gmail.com>
2025-08-21 14:32:12 +0530

    Update views.py

--- TASK4&5/Details/Employee/views.py
+++ TASK4&5/Details/Employee/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Emptable
 from .serializers import EmpSerializer
-from rest_framework import APIView
+from rest_framework.views import APIView
 
 # Create your views here.
 
