796e9fe51e03c03bd73bacf7e8f487aa465bd4d0
ekta784 <ektavora0708@gmail.com>
2025-08-21 16:20:47 +0530

    xyz3

--- TASK4&5/Details/Employee/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Emptable
-
-class EmpSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Emptable
-        fields = '__all__'
-
--- TASK4&5/Details/Employee/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import EmployeeView
-
-urlpatterns = [
-    path("employees/", EmployeeView.as_view(), name="employee-list"),
-]
