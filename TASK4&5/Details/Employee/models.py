c440ede45a425a851474add83f25630619c3ca89
ekta784 <ektavora0708@gmail.com>
2025-08-21 14:08:17 +0530

    rest framework setup

--- TASK4&5/Details/Employee/models.py
+++ TASK4&5/Details/Employee/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Emptable(models.Model):
+    id= models.IntegerField(primary_key=id)
+    name= models.CharField(max_length=20) 
+    baseSalary= models.IntegerField()
+    departmentId= models.ForeignKey()
+
+def __str__(self):
+    return self.name
+
---
+++ TASK4&5/Details/Employee/serializers.py
@@ -0,0 +1,5 @@
+from .models import Emptable
+from rest_framework import serializers
+
+
+class empserializer(serializers)
