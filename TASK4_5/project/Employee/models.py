22bbc3d08c3a2dd3fcb7407b73908c71d43a21ec
ekta784 <ektavora0708@gmail.com>
2025-08-21 17:07:23 +0530

    final

--- TASK4_5/project/Employee/models.py
+++ TASK4_5/project/Employee/models.py
@@ -1,23 +1,17 @@
 from django.db import models
-import uuid
-
-
-# Create your models here.
-class Deptable(models.Model):
-    id= models.UUID(primary_key=True, default=uuid.uuid4, editable=False)
-    name= models.CharField(max_length=20) 
-
-
-def __str__(self):
-    return self.name    
-
 
 class Emptable(models.Model):
-    id= models.IntegerField(primary_key=True)
-    name= models.CharField(max_length=20) 
-    baseSalary= models.IntegerField()
-    departmentId= models.ForeignKey(Deptable, on_delete=models.CASCADE)
+     id= models.IntegerField(primary_key=True)
+     name= models.CharField(max_length=20)
+     baseSalary= models.IntegerField() 
+     departmentId= models.IntegerField()
 
 def __str__(self):
-    return self.name
+       return self.name
 
+class Deptable(models.Model): 
+    id= models.IntegerField(primary_key=True) 
+    name= models.CharField(max_length=20)
+
+def __str__(self):
+       return self.name
