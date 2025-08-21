bd391b83c8414c4e562c462041b23d316fb39824
ekta784 <ektavora0708@gmail.com>
2025-08-21 14:09:54 +0530

    rest framework

--- TASK4&5/Details/Employee/serializers.py
+++ TASK4&5/Details/Employee/serializers.py
@@ -1,5 +1,7 @@
-from .models import Emptable
 from rest_framework import serializers
+from .models import Student   # your app's model
 
-
-class empserializer(serializers)
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
